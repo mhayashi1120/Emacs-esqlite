b28b06eb0f057988143b305de856610ad3a3211e
Masahiro Hayashi <mhayashi1120@gmail.com>
2014-01-19 23:51:58 +0900

    temporary fix.

--- esqlite.el
+++ esqlite.el
@@ -393,6 +393,8 @@ Normally, no need to use this parameter.")
      ;; currently no meanings of this
      ;; in the future release may support i18n.
      (setenv "LANG" "C")
+     ;; TODO
+     (setenv "TERM" "dumb")
      ,@form))
 
 (defmacro esqlite--with-process (proc &rest form)
